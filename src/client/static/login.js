232fa62a5e32f77d826e0ca2a48addf8cf3784bb
Andy Christianson <git@andyic.org>
2011-04-29 17:56:31 -0700

    cleaned up appearance and added openid providers

--- src/client/static/login.js
+++ src/client/static/login.js
@@ -1,7 +1,37 @@
 $( function() {
+	var openIdProviders = [
+		{
+			"name": "Google",
+			"image": "google.png",
+			"endpoint": "https://www.google.com/accounts/o8/id"
+		},
+		{
+			"name": "Yahoo",
+			"image": "yahoo.png",
+			"endpoint": "https://me.yahoo.com"
+		},
+		{
+			"name": "MyOpenID",
+			"image": "myopenid.png",
+			"endpoint": "http://myopenid.com/"
+		},
+	];
 	$( "#google-button" ).click( function() {
-		$( "#openid-input" ).val( "https://www.google.com/accounts/o8/id" );
-		$( "#submit-button" ).click();
+	} );
+
+	$( "#openid_list" ).each( function( i, openIdListEl ) {
+		var $openIdList = $( openIdListEl );
+		$( openIdProviders ).each( function( i, openIdProvider ) {
+			$openIdList.append(
+				$( "<a/>" )
+					.click( function() {
+						$( "#openid-input" ).val( openIdProvider["endpoint"] );
+						$( "#submit-button" ).click();
+					} ).append(
+						$( "<img/>" ).attr( "src", "/litcomp-multi/static/img/" + openIdProvider["image"] )
+					)
+			);
+		} );
 	} );
 
 	if ( serverData && serverData["errorMessage"] ) {
